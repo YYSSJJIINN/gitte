39cbb0c238d7676b692d64a5e4297953f05ff3a7
YYSSJJIINN <h20171058@daum.net>
2025-03-02 14:57:55 +0900

    feat: index 파일 메소드 요청 파일 POST #이슬기

---
+++ gitte-main/test404project/src/main/java/org/example/test404project/Test404projectApplication.java
@@ -0,0 +1,13 @@
+package org.example.test404project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test404projectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test404projectApplication.class, args);
+    }
+
+}
---
+++ gitte-main/test404project/src/test/java/org/example/test404project/Test404projectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.test404project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test404projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
